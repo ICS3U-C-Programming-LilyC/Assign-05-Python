3e7adfe1e1e937dd7372e606aa13cb5eb26fa697
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-12-08 21:33:52 +0000

    Code works

--- pythagorean_theorem.py
+++ pythagorean_theorem.py
@@ -12,9 +12,11 @@ import math
 # Declaring function to calculate the hypotenuse.
 def calculate_hypotenuse(side_a, side_b):
     # Calculating the hypotenuse.
-    hypotenuse = math.sqrt(side_a**2 + side_b**2)
+    sum_of_squares = side_a**2 + side_b**2
+    hypotenuse = math.sqrt(sum_of_squares)
     # Returning the hypotenuse to the function.
     return hypotenuse
+
 
 # Declaring function to calculate the perimeter.
 def calculate_perimeter(side_a, side_b):
@@ -25,38 +27,50 @@ def calculate_perimeter(side_a, side_b):
     # Returning the perimeter to the function.
     return perimeter
 
+
 # Declaring main() function to get user input/output and catching invalid inputs.
 def main():
-    # Getting user input for the 2 sides (A & B).
-    side_a = float(input("Enter the length of side A (cm): "))
-    side_b = float(input("Enter the length of side B (cm): "))
+    # Getting user using a while True loop for the sides (A & B).
+    while True:
+        # Using a try catch to catch erroneous data.
+        try:
+            # Converting user input from strings to floats.
+            side_a = float(input("Enter the length of side A (cm): "))
+            side_b = float(input("Enter the length of side B (cm): "))
 
-    # Using a try catch to catch any erroneous inputs.
-    try:
-        side_a_as_float = float(side_a)
-        side_b_as_float = float(side_b)
-        # Checking if the input is greater than 0.
-        if side_a_as_float > 0 and side_b_as_float > 0:
-            # Calling functions to display hypotenuse and perimeter.
-            hypotenuse = calculate_hypotenuse(side_a_as_float, side_b_as_float)
-            perimeter = calculate_perimeter(side_a_as_float, side_b_as_float)
-            # Displaying the hypotenuse and perimeter to the user.
-            print("The hypotenuse is {:.2F}".format(hypotenuse))
-            print("The perimeter is {:.2F}".format(perimeter))
+            # If statement to check if the inputs are greater than or equal to 0.
+            if side_a >= 0 and side_b >= 0:
+                # If they're less than 0 or equal to 0, then break out of the loop.
+                break
+            # Else their input is less than or equal to 0.
+            else:
+                print("Please enter a number greater than 0.")
 
-        # Else the input is not greater than 0.
-        else:
-            print("Please enter a number greater than 0.")
+        # Catching any errors.
+        except:
+            print("Invalid input. Please enter a valid number.")
 
-    # Catching any errors.
-    except:
-        print("Invalid input.")
+    # Calling functions to display hypotenuse and perimeter.
+    hypotenuse = calculate_hypotenuse(side_a, side_b)
+    perimeter = calculate_perimeter(side_a, side_b)
 
-# Using a while true loop to ask the user if they want to run my program again.
-while True:
-    main()
-    if input("Do you want to run my program again? (1 - Yes or 2 - No): ") != "1":
-        break
+    # Displaying the hypotenuse and perimeter to the user.
+    print("The hypotenuse is {:.2F}".format(hypotenuse))
+    print("The perimeter is {:.2F}".format(perimeter))
 
-if __name__ == "__main__":
-    main()
+
+# Declaring a function for the loop.
+def run_program():
+    # Using a while loop to ask the user if they want to run the program again.
+    while True:
+        # Loop main() function.
+        main()
+        # Asking user if they want to run my program again.
+        user_input = input("Do you want to run the program again? (1 -Yes or 2 - No): ")
+        # If the user input does not equal 1 then break out of the loop.
+        if user_input != "1":
+            break
+
+
+# Calling the run_program() function to execute the loop.
+run_program()
